337da3183ad888a287bce8899d4062865ef105ed
Randy Halim <randy.halim@outlook.com>
2020-12-16 22:11:45 +0000

    Fix failing test

--- test/item.test.ts
+++ test/item.test.ts
@@ -1,22 +1,19 @@
-import { boolean, number, string } from 'zod';
+import { string } from 'zod';
 import Item from '../src/lib/item';
 
+const item = new Item(
+  'TEST_TABLE',
+  {
+    stringProp: string().max(3),
+  },
+  ['stringProp']
+);
+
 it('Creates an item without error', () => {
-  expect(
-    new Item({
-      stringProp: string(),
-      numberProp: number(),
-      booleanProp: boolean(),
-    })
-  ).toBeInstanceOf(Item);
+  expect(item).toBeInstanceOf(Item);
 });
 it('Correctly returns parsed value and throws for invalid', () => {
-  const myItem = new Item({
-    stringProp: string().max(3),
-  });
-  expect(myItem.create({ stringProp: 'foo' })).toMatchObject({
-    stringProp: 'foo',
-  });
-  expect(myItem.create.bind(myItem, { stringProp: 'longer' })).toThrow();
-  expect(myItem.create.bind(myItem, { stringProp: true })).toThrow();
+  expect(item.create.bind(item, { stringProp: 'foo' })).not.toThrow();
+  expect(item.create.bind(item, { stringProp: 'longer' })).toThrow();
+  expect(item.create.bind(item, { stringProp: true })).toThrow();
 });
