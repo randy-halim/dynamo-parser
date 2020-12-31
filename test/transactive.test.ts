33274c579b13702a10360bb5d321999f760dcee6
Randy H <randy.halim@outlook.com>
2020-12-30 18:53:22 -0800

    Set up testing for transactive

---
+++ test/transactive.test.ts
@@ -0,0 +1,23 @@
+// import Item, { Transactive } from '../src/index';
+// import z from 'zod';
+// import DocumentClient from './document-client.config';
+
+// const $Book = new Item(
+//   'my-table',
+//   {
+//     id: z.string().max(5),
+//     bookName: z.string(),
+//     rating: z
+//       .number()
+//       .min(1)
+//       .max(5),
+//   },
+//   ['id'],
+//   DocumentClient
+// );
+
+describe('Transactive Get Operations', () => {
+  it('Need to make tests', () => {
+    expect(true).toBe(true);
+  });
+});
